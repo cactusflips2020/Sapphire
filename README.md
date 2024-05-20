ec8bd2cefb2d11a265426ee3f758ef36eaa2a3e2
Steven Sadecki <steven.sadecki@gmail.com>
2024-05-20 16:29:38 -0500

    removedcrap

--- README.md
+++ README.md
@@ -2,15 +2,25 @@
 <img src="/public/img/logo.png" style="width: 250px;"/>
 <h1>Sapphire</h1>
 <br/>
-Sapphire is a web proxy built on Ultraviolet that has lightning fast speeds so you can browse the web without delay.
+Sapphire is a lightning fast web-proxy built on Ultraviolet for browsing the web without delay.
   <br/>
   <br/>
 </div>
 <h1>Features</h1>
 <ul>
-<li>Lightning fast proxy speeds.</li>
-<li>Games and apps.</li>
-<li>Customizable settings.</li>
-<li>No malicious ads.</li>
+<li>Sleek and clean UI</li>
+<li>about:blank cloaking</li>
+<li>Lightning fast proxy speeds</li>
+<li>No ads</li>
 </ul>
+<h1>Deployment</h1>
+<h2>Deploy with Koyeb</h2>
+1. Make a <a href="https://www.koyeb.com/">Koyeb</a> account
 <br>
+2. Visit <a href="https://app.koyeb.com/apps/deploy?type=git&repository=github.com/cactusflips2020/Sapphire">this link</a>
+<br>
+3. Change the port from 8000 to 3000 and the region to whatever is closest to you
+<br>
+4. Click deploy
+<br>
+Once done with steps wait for it to deploy and then click the link provided
--- index.js
+++ index.js
@@ -21,11 +21,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/gxmes', (req, res) => {
-  res.render("gxmes", { title: "Geometry" });
+  res.render("gxmes", { title: "Games" });
 });
 
 app.get('/apps', (req, res) => {
-  res.render("apps", { title: "Algebra" });
+  res.render("apps", { title: "Apps" });
 });
 
 app.get('/settings', (req, res) => {
@@ -37,44 +37,20 @@ app.get('/extra', (req, res) => {
 });
 
 app.get('/textbook', (req, res) => {
-  res.render('loader',  { title: "Textbook" });
+  res.render('loader',  { title: "Loading..." });
 });
 
 app.get('/learning', (req, res) => {
   res.render('learning');
 });
 
-app.get('/mobile', (req, res) => {
-  res.render('mobile',  { title: "Mobile" });
-});
-
 app.get('/e/e/e/testingfortnite012948', (req, res) => {
   res.render('extra/testing');
 });
 
-app.get('/dashboard', (req, res) => {
-  res.render('dashboard', { title: "Dashboard" });
-});
-
-app.get('/teachers', (req, res) => {
-  res.render('teachers');
-});
-
-app.get('/tos', (req, res) => {
-  res.render('tos');
-});
-
-app.get('/learningMB', (req, res) => {
-  res.render('learningmobile')
-});
-
-app.get('/byod', (req, res) => {
-  res.render('aboutus')
-});
-
 app.use((req, res) => {
   res.statusCode = 404;
-  res.render("404", { title: "404 | Error", error: "It looks like the page you were looking for doesn't exist.",});
+  res.render("404", { title: "Sapphire | 404", error: "It looks like the page you were looking for doesn't exist.",});
 });
 
 server.on("request", (req, res) => {
@@ -94,7 +70,7 @@ server.on("upgrade", (req, socket, head) => {
 });
 
 server.on("listening", () => {
-  console.log(`Kitty Cat Club is running on: http://localhost:${port}`);
+  console.log(`Sapphire is running on: http://localhost:${port}`);
 });
 
 server.listen({
